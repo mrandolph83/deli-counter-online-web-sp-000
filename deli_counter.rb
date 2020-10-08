8a6892791c8de24c387fc0287bafdeaeb65ee8cc
Matt Smith <mrandolph83@gmail.com>
2020-10-08 15:56:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ katz_deli = []
 def line(katz_deli)
   # Shows everyone current place in line. If there is nobody in line,
   #it should say "The line is currently empty.".
-  if katz_deli != nil
+  if katz_deli !== nil
 
     katz_deli.each.with_index(1) do |person, index|
     puts "The line is currently #{index}. #{person}"
